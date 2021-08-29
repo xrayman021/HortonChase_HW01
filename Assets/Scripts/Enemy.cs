d95bd04b03377146303e88bcebbf3f5ca66601d2
Chase's Hub <chaseghorton99@gmail.com>
2021-08-29 16:12:56 -0500

    Coded Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int _damageAmount = 1;
+    [SerializeField] ParticleSystem _impactParticles;
+    [SerializeField] AudioClip _impactSound;
+
+    Rigidbody _rb;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        Player player = other.gameObject.GetComponent<Player>();
+        if(player != null)
+        {
+            PlayerImpact(player);
+            ImpactFeedback();
+        }
     }
+
+    public void PlayerImpact(Player player)
+    {
+        player.DecreaseHealth(_damageAmount);
+    }
+
+    private void ImpactFeedback()
+    {
+        //particles
+        if (_impactParticles != null)
+        {
+            _impactParticles = Instantiate(_impactParticles, transform.position, Quaternion.identity);
+        }
+        //audio TODO - consider object pooling for performance
+        if (_impactSound != null)
+        {
+            AudioHelper.PlayClip2D(_impactSound, 1f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+
+    }
+
 }

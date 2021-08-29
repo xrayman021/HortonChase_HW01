0b5a7298f973b93f3df9e47780ce852cae8b7116
Chase's Hub <chaseghorton99@gmail.com>
2021-08-29 15:27:08 -0500

    Coded Player Health and Kill state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TankController))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int _maxHealth = 3;
+    int _currentHealth;
+
+    TankController _tankController;
+
+    private void Awake()
     {
-        
+        _tankController = GetComponent<TankController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _currentHealth = _maxHealth;
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        Debug.Log("Player's health: " + _currentHealth);
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        Debug.Log("Player's health: " + _currentHealth);
+        if(_currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+        //Play particles
+        //Play sounds
     }
 }
